a97f4a2e40531e46fac417625e7d059a6ee8dab8
Astrash <astrash@protonmail.com>
2021-12-21 10:38:44 +1100

    Automate dev wiki

--- src/sync.sh
+++ src/sync.sh
@@ -16,5 +16,5 @@ git add *
 git commit -m "GitHub Action Deployment"
 
 echo "Pushing to wiki..."
-git remote add origin https://${ACCESS_TOKEN}@github.com/PolyhedralDev/Terra.wiki.git
+git remote add origin https://${ACCESS_TOKEN}@github.com/PolyhedralDev/$1
 git push origin master --force
